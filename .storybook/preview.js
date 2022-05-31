dc150763e02906ecc56551a63aeb101afeebcfa9
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2022-05-31 13:40:54 +0100

    boilerplate for tailwindcss & storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,41 @@
+import '../styles/globals.css'
+import * as NextImage from 'next/image'
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
+
+const BREAKPOINTS_INT = {
+  sm: 1000,
+  md: 1280,
+  lg: 99999,
+}
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ]
+  })
+)
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: { viewports: customViewports },
+}
