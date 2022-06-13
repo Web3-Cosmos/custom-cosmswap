0fbb19fa2b9c89fad3310de95c680a4891671b11
Top-Dev-Ops <benjamin.minato.geek@gmail.com>
2022-06-13 22:43:03 +0100

    fix: minor fix

--- hooks/application/connector/index.ts
+++ hooks/application/connector/index.ts
@@ -28,6 +28,5 @@ export const injected = new InjectedConnector({
 export const walletconnect = new WalletConnectConnector({
   rpc: { 1: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
-  qrcode: true,
-  pollingInterval: 15000,
+  qrcode: true
 })
